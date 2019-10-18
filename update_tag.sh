e556a0639068e65c2dcb28508cd604da916d007f
Allan Henrique <allan.rodrigues@matera.com>
2019-10-18 16:26:52 -0300

    ajuste para o job do Jenkins

--- update_tag.sh
+++ update_tag.sh
@@ -2,66 +2,39 @@
 
 if [ $# -eq 0 ]
   then
-    echo "Você deveria informar uma versão"
+    echo 'VocÃª deveria informar ao menos uma versÃ£o "-v <vxx>"'
     exit 1
 fi
 
+set -x
 VERSION=''
-REPO=''
+REPO='origin'
+TAG_CONTENT='"default value"'
 
-while getopts ":v:r:" opt; do
+
+while getopts ":v:r:t:" opt; do
   case $opt in
     v) VERSION="$OPTARG"
     ;;
     r) REPO="$OPTARG"
+    ;;
+    t) TAG_CONTENT="$OPTARG"
     ;;
     \?) echo "Invalid option -$OPTARG" >&2
     ;;
   esac
 done
 
-TAG_NAME='${VERSION}-latest'
+TAG_NAME=${VERSION}
 
+# delete tag local
+git tag -d ${TAG_NAME}
 
-# delete local tag '12345'
-git tag -d $TAG_NAME
-# delete remote tag '12345' (eg, GitHub version too)
-git push --delete $REPO $TAG_NAME
+# deleta tag remoto
+git push --delete ${REPO} ${TAG_NAME}
 
+# cria nova tag baseada no nome informado e com conteudo descrito em ${TAG_CONTENT}
+git tag -a ${TAG_NAME} -m ${TAG_CONTENT}
 
-git tag -a $TAG_NAME
-
-git push origin $TAG_NAME#!/bin/bash
-
-if [ $\# -eq 0 ]
-  then
-    echo 'Você deveria informar uma versão'
-    exit 1
-fi
-
-VERSION=''
-REPO=''
-
-while getopts ":v:r:" opt; do
-  case  in
-    v) VERSION=""
-    ;;
-    r) REPO=""
-    ;;
-    \?) echo "Invalid option -" >&2
-    ;;
-  esac
-done
-
-TAG_NAME='-latest'
-
-
-# delete local tag '12345'
-git tag -d 
-# delete remote tag '12345' (eg, GitHub version too)
-git push --delete  
-
-
-git tag -a 
-
-git push origin 
+# empurra a alteraÃ§Ã£o para o remoto
+git push ${REPO} ${TAG_NAME}

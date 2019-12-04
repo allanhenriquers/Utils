3ce20f289edf19c607d3845821b715b1b48ba98b
Allan Henrique <allan.rodrigues@matera.com>
2019-12-04 09:27:47 -0300

    criação do 'criador de changelog'

--- .scripts/changeLogger_prepare.sh
+++ .scripts/changeLogger_prepare.sh
@@ -7,18 +7,18 @@ echo alias clogger=\'~/.scripts/changeLogger.sh\' >> ~/.bashrc
 cd ~/.scripts
 touch changeLogger.sh
 sudo chmod +x changeLogger.sh
-echo 
-" #!/bin/sh
+source ~/.bashrc
+echo "#!/bin/sh
 
-while getopts \":o:d:s:\" opt; do
+while getopts ':o:d:s:' opt; do
   case \$opt in
-    o) OLD_TAG=\"\$OPTARG\"
+    o) OLD_TAG='\$OPTARG'
     ;;
-    d) NEW_TAG=\"\$OPTARG\"
+    d) NEW_TAG='\$OPTARG'
     ;;
-    s) SISTEMA=\"\$OPTARG\"
+    s) SISTEMA='\$OPTARG'
     ;;
-    \?) echo \"Invalid option -\$OPTARG\" >&2
+    \?) echo 'Invalid option -\$OPTARG' >&2
     ;;
   esac
 done
@@ -27,9 +27,7 @@ LOG_PATH='./CHANGELOG.md'
 
 echo 'Tag do '\${SISTEMA}':\n    '\${NEW_TAG}'\nDemandas:'  > \${LOG_PATH}
 
-git log --pretty=format:'    %s' \${OLD_TAG}..${NEW_TAG} | grep -v -e 'Updating'
--e 'Merging' -e 'Releasing' -e 'Merge' -e 'Automati'  >> \${LOG_PATH}
+git log --pretty=format:'    %s' \${OLD_TAG}..\${NEW_TAG} | grep -v -e 'Updating' -e 'Merging' -e 'Releasing' -e 'Merge' -e 'Automati'>> \${LOG_PATH}
 
-echo 'Entregue em:\n''    '`date` >> \${LOG_PATH} " >> ~/.scripts/changeLogger.sh
+echo 'Entregue em:\\\n''    '\`date\` >> \${LOG_PATH} " > ~/.scripts/changeLogger.sh
 
-source ~/.bashrc
